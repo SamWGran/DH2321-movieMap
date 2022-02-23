8e6d7a290268c7b7ae79d91e7af1ae557385c6a1
lowe.bonnevier@gmail.com <lowe.bonnevier@gmail.com>
2022-02-23 04:16:21 +0100

    Updated App dimensions.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Moviemap from './classes/Moviemap';
 function App() {
   return (
     <div className="App">
-      <Moviemap id="moviemap" width={1200} height={900}/>
+      <Moviemap id="moviemap" width={1000} height={800}/>
     </div>
   );
 }
